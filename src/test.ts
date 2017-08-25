7e9150bc44a8804692f34adbca09f34ac3d1707b
Alex Bepple <ab@Marc-Uwe.local>
2017-08-25 21:22:16 +0200

    $ evolve grid with one live cell

--- src/test.ts
+++ src/test.ts
@@ -2,13 +2,19 @@ import {assertThat, is} from 'hamjest'
 import 'mocha'
 import r = require('ramda')
 
-const evolve = r.identity
+const emptyGrid = []
+const evolve = r.always(emptyGrid)
 
 describe('Next generation of', () => {
   describe('empty grid', () => {
     it('is empty grid', () => {
-      const emptyGrid = []
       assertThat(evolve(emptyGrid), is(emptyGrid))
+    })
+  })
+  describe('grid with one live cell', () => {
+    it('is empty grid', () => {
+      const gridWithOneLiveCell = [[0,0]]
+      assertThat(evolve(gridWithOneLiveCell), is(emptyGrid))
     })
   })
 })
