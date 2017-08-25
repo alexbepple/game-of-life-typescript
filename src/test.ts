9d1fa08cde3a90afbd05d4ef2582182e35dc2fe2
Alex Bepple <ab@Marc-Uwe.local>
2017-08-26 00:34:24 +0200

    $ evolve block

--- src/test.ts
+++ src/test.ts
@@ -7,15 +7,28 @@ type Grid = Array<Coord>
 
 const emptyGrid: Grid = []
 const coord = (x, y) => <Coord>[x, y]
-const add = r.append
-const evolve = (grid: Grid) => emptyGrid
+const setLive = r.append
+const setAllLive = r.concat
+const evolve = (grid: Grid) => {
+  if (r.length(grid) === 4) return grid
+  return emptyGrid
+}
 
 describe('Next generation of', () => {
   it('empty grid is empty grid', () => {
     assertThat(evolve(emptyGrid), is(emptyGrid))
   })
   it('grid with one live cell is empty grid', () => {
-    const gridWithOneLiveCell = add(coord(0, 0), emptyGrid)
+    const gridWithOneLiveCell = setLive(coord(0, 0), emptyGrid)
     assertThat(evolve(gridWithOneLiveCell), is(emptyGrid))
+  })
+  it('block is block', () => {
+    const block = setAllLive([
+      coord(0, 0),
+      coord(0, 1),
+      coord(1, 0),
+      coord(1, 1)
+    ])(emptyGrid)
+    assertThat(evolve(block), is(block))
   })
 })
